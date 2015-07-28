c9b648118b7a8765eed9bd10db57616c55cea75d
yuzhixuan007 <yuzhixuan007@kanoutekiiMac.local>
2015-07-28 23:09:45 +0800

    1212

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yiming
+//
+//  Created by 叶 楠 on 15/7/26.
+//  Copyright (c) 2015年 reflynet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yiming
+//
+//  Created by 叶 楠 on 15/7/26.
+//  Copyright (c) 2015年 reflynet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
