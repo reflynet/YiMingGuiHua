e9e392e27d7797923765941cce1ed9226b596dfd
攒钱网 <zanqianwang@zanqianwangdeMacBook-Air.local>
2015-08-13 15:55:16 +0800

    t

---
+++ DetialViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetialViewController.h
+//  yiming
+//
+//  Created by 攒钱网 on 15/8/13.
+//  Copyright (c) 2015年 reflynet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetialViewController : UIViewController
+
+@end
